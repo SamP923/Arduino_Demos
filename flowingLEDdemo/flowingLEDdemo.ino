3a6dd7ac9ce8a50a90b728092b02968bc625802b
Sam P <33042752+SamP923@users.noreply.github.com>
2019-10-12 16:20:01 -0700

    6 led demo

--- flowingLEDdemo/flowingLEDdemo.ino
+++ flowingLEDdemo/flowingLEDdemo.ino
@@ -1,21 +1,16 @@
-//Flowing LED Lights
-/* Five LEDs will light up one by one from left to right, and then go out one by one from right to left.
-After that, the LEDs will light up one by one from right to left, and then go out one by one from left to right.
-This process will repeat indefinitely.*/
-
 /**************************************/
 const int lowestPin = 2;//the lowest one attach to
-const int highestPin = 6;//the highest one attach to
+const int highestPin = 7;//the highest one attach to
 /**************************************/
 void setup()
 {
-  //set pins 1 through 6 as output
+  //set pins 2 through 7 as output
   for(int thisPin = lowestPin;thisPin <= highestPin;thisPin++)
   {
     pinMode(thisPin,OUTPUT); //initialize thisPin as an output
   }
 }
-/****************************************/
+
 void loop()
 {
   //iterate over the pins
@@ -31,11 +26,14 @@ void loop()
     digitalWrite(thisPin,LOW);//turn this led off
     delay(100);//wait for 100 microseconds
   }
+
+  //turn the led on from highest to lowest
   for(int thisPin = highestPin;thisPin>=lowestPin;thisPin--)
   {
     digitalWrite(thisPin,HIGH);//turn this led on
     delay(100);//wait for 100 microseconds
   }
+  //fade from lowest to highest 
   for(int thisPin = lowestPin;thisPin <= highestPin;thisPin++)
   {
     digitalWrite(thisPin,LOW);//turn this led off
